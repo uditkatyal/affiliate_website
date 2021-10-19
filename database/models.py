9a59f520b6ca0a1c9890ddff1a232bbab1f11ccd
Ayush Verma <ayush02av@gmail.com>
2021-10-19 12:49:40 +0530

    Update

--- database/models.py
+++ database/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	content = models.TextField()
+	affiliate_link = models.TextField()
+
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	tags = models.ManyToManyField(Tag, blank=True)
+
+	views = models.IntegerField(default=0)
+
+class Blog(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	tags = models.ManyToManyField(Tag, blank=True)
+
+	views = models.IntegerField(default=0)
+
+class Email(models.Model):
+	email = models.CharField(primary_key=True, max_length=100)
