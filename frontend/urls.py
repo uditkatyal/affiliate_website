09e817b8e4ad082f477f8e8fc1bca804347a2748
Ayush Verma <ayush02av@gmail.com>
2021-10-20 16:37:21 +0530

    update

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path 
+from .views import *
+
+urlpatterns = [
+	path('', index),
+	path('<str:page_request>/', render_page)
+]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'index.html')
+
+def render_page(request, page_request):
+	page_request = page_request.replace(".html", "")
+
+	if page_request == 'favicon.ico':
+		return HttpResponse('')
+
+	try:
+		return render(request, f'{page_request}.html')
+	except:
+		return redirect(index)
